6749c9f0b499390615bdeb1adff419a2ff3de9c8
Bhashak <158458992+Bhashak@users.noreply.github.com>
2024-06-06 11:55:17 +0530

    Update DQG_Response.java

--- src/test/java/Response/DQG_Response.java
+++ src/test/java/Response/DQG_Response.java
@@ -1,33 +1,15 @@
 package Response;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-
-import org.apache.commons.mail.EmailException;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.testng.annotations.Test;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.CellType;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import io.github.bonigarcia.wdm.WebDriverManager;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.mail.DefaultAuthenticator;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.HtmlEmail;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+import kong.unirest.HttpResponse;
+import kong.unirest.Unirest;
 
 public class DQG_Response {
 
@@ -36,8 +18,8 @@ public class DQG_Response {
 	public String login_responsebody;
 	public String ConnectionList_responsebody;
 	
-	 // public static String pincode = "";
-	    // static WebDriver driver;
+	 public static String pincode = "";
+	    static WebDriver driver;
 	@Test
 	public void Login_and_ConnectionList() throws EmailException, IOException, InterruptedException
 	{
@@ -74,83 +56,83 @@ public class DQG_Response {
 			                email.setContent("Hi Team.<br>DQG Working Status:<br>" +"Login status : "+login_status + "<br>Connections List Status: " +ConnectionList_status , "text/html");
 			                email.addCc("lakshminarayana.g@kairostech.com");
 			                email.addCc("bhasha.k@kairostech.com");
-			     	      	email.addTo("ravikumar.p@kairostech.com");			                
+			     	      	email.addTo("ravikumar.p@kairostech.com");
+//			                
 			                email.send();
 	}
 		
 		
 		
-	}
-	
-
 		
-		// WebDriverManager.edgedriver().setup();
-  //       driver = new EdgeDriver();
-  //       driver.manage().window().maximize();
+	
+//
+//		  WebDriverManager.edgedriver().setup();
+//	       driver = new EdgeDriver();
+//	        driver.manage().window().maximize();
+//
+//	        FileInputStream excelFile = new FileInputStream("D:\\MyWorkSpace\\DQG_Workspace\\DQG\\src\\test\\resources\\1.xlsx");
+//	        Workbook workbook = new XSSFWorkbook(excelFile);
+//	        Sheet sheet = workbook.getSheetAt(0);
+//        for (int rowNum = 1; rowNum <= sheet.getLastRowNum(); rowNum++) {
+//            Row row = sheet.getRow(rowNum);
+//            String country = row.getCell(1).getStringCellValue();
+//            String state = row.getCell(2).getStringCellValue();
+//            String city = row.getCell(3).getStringCellValue();
+//
+//            driver.get("https://www.bing.com/");
+//            Thread.sleep(3000);
+//            driver.findElement(By.xpath("//textarea[@name='q']")).sendKeys(country + "," + state + "," + city + " ,pincode", Keys.ENTER);
+//
+//            try {
+//                WebElement pincodeElement = driver.findElement(By.xpath("//div[@class='b_focusTextLarge' or @class='b_focusTextMedium']"));
+//                pincode = pincodeElement.getText();//div[@class='b_focusTextLarge' or @class='b_focusTextMedium']
+//              System.out.println(pincode);
+//            } catch (NoSuchElementException e) {
+//            System.out.println("First element not found at Row Number: " + rowNum + ", trying next element.");
+//                try {
+//                    WebElement element1 = driver.findElement(By.xpath("//p[@class='b_lineclamp3 b_algoSlug']//strong[1]"));
+//                    if (element1.isDisplayed()) {
+//                        pincode = element1.getText();
+//                        System.out.println(pincode);
+//                    }
+//                } catch (NoSuchElementException e1) {
+//                    System.out.println("Second element not found at Row Number: " + rowNum + ", trying next element.");
+//                    try {
+//                        WebElement element2 = driver.findElement(By.xpath("//*[@id=\"b_results\"]/li[1]/h2/a"));
+//                        WebElement element3 = driver.findElement(By.xpath("//span[@style='font-size:250%; font-weight:bold;']"));
+//                        if (element2.isDisplayed()) {
+//                            JavascriptExecutor js = (JavascriptExecutor) driver;
+//                            js.executeScript("arguments[0].removeAttribute('target');", element2);
+//                            element2.click();
+//                            Thread.sleep(2000);
+//                            pincode=element3.getText();
+//                            System.out.println(pincode);
+//                            driver.navigate().back();
+//                        }
+//                    } catch (NoSuchElementException e2) {
+//                        System.out.println("All elements not found at Row Number: " + rowNum + ", setting pincode as 'Not Found'.");
+//                        pincode = "Not Found";
+//                    }
+//                }
+//            }
+//
+//            Cell pincodeCell = row.createCell(7, CellType.STRING);  // Column index 7 corresponds to the 8th column
+//            pincodeCell.setCellValue(pincode);
+//        }
+//
+//        FileOutputStream outputStream = new FileOutputStream("D:\\MyWorkSpace\\DQG_Workspace\\DQG\\src\\test\\resources\\5.xlsx");
+//        workbook.write(outputStream);
+//        workbook.close();
+//        outputStream.close();
+//        excelFile.close();
+//        driver.quit();
+//    }
+//
+//    public static void scrolltoEle(WebElement ele) {
+//        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", ele);
+//    }
 
-  //       FileInputStream excelFile = new FileInputStream(".//src//main//Resources//Book1.xlsx");
-  //       Workbook workbook = new XSSFWorkbook(excelFile);
-  //       Sheet sheet = workbook.getSheetAt(0);
-
-  //       for (int rowNum = 1; rowNum <= sheet.getLastRowNum(); rowNum++) {
-  //           Row row = sheet.getRow(rowNum);
-  //           String country = row.getCell(1).getStringCellValue();
-  //           String state = row.getCell(2).getStringCellValue();
-  //           String city = row.getCell(3).getStringCellValue();
-
-  //           driver.get("https://www.bing.com/");
-  //           Thread.sleep(3000);
-  //           driver.findElement(By.xpath("//textarea[@name='q']")).sendKeys(country + "," + state + "," + city + " ,pincode", Keys.ENTER);
-
-  //           try {
-  //               WebElement pincodeElement = driver.findElement(By.xpath("//div[@class='b_focusTextLarge' or @class='b_focusTextMedium']"));
-  //               pincode = pincodeElement.getText();//div[@class='b_focusTextLarge' or @class='b_focusTextMedium']
-  //             System.out.println(pincode);
-  //           } catch (NoSuchElementException e) {
-  //           System.out.println("First element not found at Row Number: " + rowNum + ", trying next element.");
-  //               try {
-  //                   WebElement element1 = driver.findElement(By.xpath("//p[@class='b_lineclamp3 b_algoSlug']//strong[1]"));
-  //                   if (element1.isDisplayed()) {
-  //                       pincode = element1.getText();
-  //                       System.out.println(pincode);
-  //                   }
-  //               } catch (NoSuchElementException e1) {
-  //                   System.out.println("Second element not found at Row Number: " + rowNum + ", trying next element.");
-  //                   try {
-  //                       WebElement element2 = driver.findElement(By.xpath("//*[@id=\"b_results\"]/li[1]/h2/a"));
-  //                       WebElement element3 = driver.findElement(By.xpath("//span[@style='font-size:250%; font-weight:bold;']"));
-  //                       if (element2.isDisplayed()) {
-  //                           JavascriptExecutor js = (JavascriptExecutor) driver;
-  //                           js.executeScript("arguments[0].removeAttribute('target');", element2);
-  //                           element2.click();
-  //                           Thread.sleep(2000);
-  //                           pincode=element3.getText();
-  //                           System.out.println(pincode);
-  //                           driver.navigate().back();
-  //                       }
-  //                   } catch (NoSuchElementException e2) {
-  //                       System.out.println("All elements not found at Row Number: " + rowNum + ", setting pincode as 'Not Found'.");
-  //                       pincode = "Not Found";
-  //                   }
-  //               }
-  //           }
-
-  //           Cell pincodeCell = row.createCell(7, CellType.STRING);  // Column index 7 corresponds to the 8th column
-  //           pincodeCell.setCellValue(pincode);
-  //       }
-
-  //       FileOutputStream outputStream = new FileOutputStream(".//src//main//java//Resources//Book1.xlsx");
-  //       workbook.write(outputStream);
-  //       workbook.close();
-  //       outputStream.close();
-  //       excelFile.close();
-  //       driver.quit();
-  //   }
-
-  //   public static void scrolltoEle(WebElement ele) {
-  //       ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", ele);
-  //   }
-
+}
 }
 
 
